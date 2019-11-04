b46069dbe0aaa8902536971c889fcc9f92940cf2
seanaleid <seanaleid@gmail.com>
2019-11-04 17:12:35 -0500

    GET /, GET /:id, POST / all functional

--- data/accounts/account-router.js
+++ data/accounts/account-router.js
@@ -22,12 +22,27 @@ router.get('/:id', (req, res) => {
     knex
         .select('*')
         .from('accounts')
-        .then(accounts => {
-            res.status(200).json(accounts);
+        .where('id', '=', req.params.id)
+        .first()
+        .then(account => {
+            res.status(200).json(account);
         })
         .catch(err => {
             res.status(500).json({ error: 'Failed to get posts from database'})
         })
 })
+
+router.post('/', (req, res) => {
+
+    knex    
+        .insert(req.body, 'id') 
+        .into('accounts')
+        .then(id => {
+            res.status(201).json(id);
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'Failed to insert post to database'})
+        })
+});
 
 module.exports = router;
